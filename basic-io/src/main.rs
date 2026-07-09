25840104318a39a756b243dcd255d5e39a073dd8
paulvenom <paul.c.fuller353@gmail.com>
2026-07-09 14:16:53 -0400

    rust practice project 1

---
+++ basic-io/src/main.rs
@@ -0,0 +1,13 @@
+#![allow(unused)]
+
+use std::io;
+
+fn main() {
+    println!("Who goes there?");
+    let mut name: String = String::new();
+    io::stdin().read_line(&mut name);
+
+    let enter = "You may now enter.";
+    println!("Hello there, {}. {}", name.trim_end(), enter)
+
+}
